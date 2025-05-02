cf8a144b8dec856c7c98b32c566c965224ab603d
André Pereira <andreporteira@gmail.com>
2025-05-02 16:01:18 -0300

    Mostrando erros

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -5,12 +5,22 @@ const baseURl = 'https://jsonplaceholder.typicode.com/users'
 
 export default function UserList() {
     const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
     
-useEffect(() => {
-    axios.get(baseURl).then((response) => {
-        setUsers(response.data);
-    })
-}, []);
+    useEffect(() => {
+        axios.get(baseURl).then((response) => {
+            setUsers(response.data);
+            setLoading(false);
+        }).catch((error) => {
+            setError('Error ao solicitar dados');
+            setLoading(false);
+        })
+    }, []);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>{error}</p>;
+
   return (
     <div>
       <h2>User List</h2>
